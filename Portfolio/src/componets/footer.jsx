bf62397063e58880e837d3bf14b0d58ec4351c40
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-11-01 12:16:59 -0500

    setting up footer

---
+++ Portfolio/src/componets/footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <p>&copy; 2023 Your Name</p>
+            <div>
+              <a href="https://www.linkedin.com/in/jaxson-fellhauer-6ab028299" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+              <a href="https://github.com/jaxson20" target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a href="https://www.your-3rd-party-profile.com" target="_blank" rel="noopener noreferrer">3rd Party</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
