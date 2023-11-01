f8227ad912627b73c855ac77ba19da075cc3ad70
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-10-31 22:26:19 -0500

    starting code on about me, contact, header, and navigation jsx files

---
+++ Portfolio/src/componets/aboutme.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="avatar.jpg" alt="Your Photo" />
+      <p>Your short bio goes here...</p>
+    </section>
+  );
+}
+
+export default AboutMe;
---
+++ Portfolio/src/componets/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function header() {
+    return (
+        <header>
+            <h1>Jaxson Fellhauer</h1>
+            <navigation />
+        </header>
+    );
+}
+
+export default header;
